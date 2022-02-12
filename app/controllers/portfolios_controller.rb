4a3f4dde7f7522fada3ffe372f718809ca710659
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-12 15:48:27 +0100

    Add index view for portfolio from scratch

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
 
 class PortfoliosController < ApplicationController
+  def index
+    @portfolio_items = Portfolio.all
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,7 @@ puts '5 skills created'
     subtitle: "My portfolio title #{portfolio_item} sub title",
     body: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     main_image: 'http://placehold.it/600x400',
-    thumb_image: 'http://placehold.it/3500x200'
+    thumb_image: 'http://placehold.it/350x200'
   )
 end
 
