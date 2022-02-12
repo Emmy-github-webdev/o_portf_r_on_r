4cb6400d5a76b466c30b670abf9e5a4c4ae3af67
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-12 22:14:31 +0100

    Add delete action

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -43,4 +43,13 @@ class PortfoliosController < ApplicationController
   def show
     @portfolio_item = Portfolio.find(params[:id])
   end
+
+  def destroy
+    @portfolio_item = Portfolio.find(params[:id])
+    @portfolio_item.destroy
+
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Portfolio item was successfully removed.' }
+    end
+  end
 end
