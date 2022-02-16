0c1e7c625263e60f3f8cfbac6dc42cee2b213743
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-16 20:03:21 +0100

    Implement image default for  protfolio

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,8 +3,19 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :subtitle, :body, :main_image, :thumb_image
 
+  #initialize scope method 1
   def self.reactjs
     where(subtitle: 'ReactJS')
   end
+  #initialize scope method 2
   scope :ruby_on_rails, -> {where(subtitle: 'Ruby on Rails')}
+
+  # Add default values
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "http://placehold.it/600x400"
+    self.thumb_image ||= "http://placehold.it/350x200"
+  end
+  
 end
