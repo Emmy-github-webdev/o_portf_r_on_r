57d220400083c6b172079e6fa5a165a42a20c2d8
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-22 09:18:43 +0100

    Refactor application controller for source feature

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,6 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
-  include DeviseWhitelist
-
-  before_action :set_source
-
-  def set_source
-    session[:source] = params[:q] if params[:q]
-  end
-  
+  include DeviseWhitelist 
+  include SetSource 
 end
