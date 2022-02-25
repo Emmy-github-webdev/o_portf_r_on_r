aefa2b9e350047e37afb84b39ba1525aa3e9ee0f
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-25 09:51:59 +0100

    Add authentication items to the master application layout

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,13 +2,13 @@
 
 module ApplicationHelper
   
-  def login_helper
+  def login_helper(style)
     if current_user.is_a?(User)
-      button_to "Logout", destroy_user_session_path, method: :delete
+      button_to "Logout", destroy_user_session_path, method: :delete, class: style
     else
-      (link_to "Register", new_user_registration_path) +
-      '<br>'.html_safe +
-      (link_to "Login", new_user_session_path)
+      (link_to "Register", new_user_registration_path, class: style) +
+      ' '.html_safe +
+      (link_to "Login", new_user_session_path, class: style)
     end
   end
   
