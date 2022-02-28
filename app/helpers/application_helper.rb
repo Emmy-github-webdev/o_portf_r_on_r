cb6e5639a077d6d786db2d8a99d9c1f907924601
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-02-28 08:28:18 +0100

    Add styling to show blog

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,7 +8,7 @@ module ApplicationHelper
       ' '.html_safe +
       (link_to "Login", new_user_session_path, class: style)
     else
-      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+      button_to "Logout", destroy_user_session_path, method: :delete, class: style
     end
   end
   
