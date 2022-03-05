0eb18f8c535d24a2bb48939ec8e65b6a515d016a
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-05 12:32:21 +0100

    Implement view helper to generate dynamic navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,5 +18,18 @@ module ApplicationHelper
       content_tag(:p, greeting, class: 'source-greeting')
     end
   end
+
+  def nav_helper(style, tag_type)
+nav_links = <<NAV
+<#{tag_type}><a href="#{root_path}" class="#{style}">Home</a></#{tag_type}>
+<#{tag_type}><a href="#{about_path}" class="#{style}">About Me</a></#{tag_type}>
+<#{tag_type}><a href="#{contact_path}" class="#{style}">Contact</a></#{tag_type}>
+<#{tag_type}><a href="#{blogs_path}" class="#{style}">Blogs</a></#{tag_type}>
+<#{tag_type}><a href="#{portfolios_path}" class="#{style}">Projects</a></#{tag_type}>
+NAV
+
+  nav_links.html_safe
+  end
+  
   
 end
