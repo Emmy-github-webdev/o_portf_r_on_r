e338b60b7e63b764bd10479f42627bab3539f6cd
Emmanuel Ogah <ogahemmanuel75@gmail.com>
2022-03-26 21:44:13 +0100

    Fix image display bug

--- app/models/concerns/placeholder.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Placeholder
-  extend ActiveSupport::Concern
-
-  def self.image_generator(height:, width:)
-    "https://via.placeholder.com/#{height}X#{width}"
-  end
-end
--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,11 +1,7 @@
 # frozen_string_literal: true
 
 class Skill < ApplicationRecord
-  include Placeholder
 
   validates_presence_of :title, :percent_utilized
-
-  def set_defaults
-    self.badge ||= Placeholder.image_generator(height: '250', width: '250')
-  end
+  
 end
